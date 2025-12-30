3119f3380bb9342df785eb267f4413c64618dc3e
Naaman Omar <naamanomar46@gmail.com>
2025-12-30 22:34:13 +0530

    implemented functions

--- main.py
+++ main.py
@@ -6,42 +6,54 @@ word_bank = [
     "phone", "shirt", "plant", "music", "river"
 ]
 
-word = rm.choice(word_bank)
+def get_random_word():
+    return rm.choice(word_bank)
 
-guessWord = ['_'] * len(word)
-
-attempts = 10
-guess_letters = set()
-while attempts > 0:
-    print("Current word: "+" ".join(guessWord))
-    print("Guessed letters: "+" ".join(sorted(guess_letters)))
-    guess = input("Enter a letter: ").lower()
+def display_state(attempts, guessed_letters, guess_word):
+    print(f"\nAttempts remaining: {attempts}")
+    print("Current word: "+" ".join(guess_word))
+    print("Guessed letters: "+" ".join(sorted(guessed_letters)))
     
+def get_valid_guess(guessed_letters):
     #Input check
-    if len(guess) != 1 or not guess.isalpha():
-        print(f"⚠️  Please enter a single letter only")
-        continue
+    while True:
+        guess = input("Enter a letter: ").lower()
+        if len(guess) != 1 or not guess.isalpha():
+            print(f"⚠️  Please enter a single letter only")
+        elif guess in guessed_letters:
+            print(f"⚠️  You already guessed that letter")
+        else:
+            return guess
+def update_word(word, guess_word,guess):
+    found = False
+    for i in range(len(word)):
+        if word[i] == guess:
+            guess_word[i] = guess
+            found = True
+    return  found         
+
+def play_game():
+    word = get_random_word()
+    guess_word = ['_'] * len(word)
+    guessed_letters = set()
+    attempts = 10
     
-    #Duplicate check
-    if guess in guess_letters:
-        print(f"⚠️  You already guessed that letter")
-        continue    
-    guess_letters.add(guess)
-    
-    if guess in word:
-        for i in range(len(word)):
-            if word[i] == guess:
-                guessWord[i] = guess
+    while attempts > 0:    
+        display_state(attempts,guessed_letters, guess_word)  
+
+        guess = get_valid_guess(guessed_letters)
+        guessed_letters.add(guess)
         
-        print(f"Great guess 😊")  
+        if update_word(word,guess_word,guess):
+            print(f"Great guess 😊")
+        else:
+            attempts -= 1
+            print("Wrong guess ❌")
         
-    else:
-        attempts -= 1
-        print(f"Wrong guess ❌ {attempts} attempts remaining !!")
+        if "_" not in guess_word:
+            print(f"Congratulations 🎉 You guessed the word '{word}'")
+            return
+    if attempts == 0  and "_" in guess_word:
+        print(f"Game Over!\nThe word was {word}")
         
-    if '_' not in guessWord:
-        print(f"Congratulations 🎉 You guessed the word '{word}'")
-        break
-    
-if attempts == 0  and "_" in guessWord:
-    print(f"Game Over!\nThe word was {word}")
+play_game()
